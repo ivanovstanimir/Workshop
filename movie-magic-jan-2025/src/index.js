84dcb5a3e6bad46b5a9222dc4f8039fb274db9ee
IvanovStr <163607151+IvanovStr@users.noreply.github.com>
2025-01-19 20:58:57 +0200

    Add and setup handlebars

--- movie-magic-jan-2025/src/index.js
+++ movie-magic-jan-2025/src/index.js
@@ -1,1 +1,21 @@
-console.log("iii");
+import express from "express";
+import handlebars from "express-handlebars";
+
+const app = express();
+
+app.engine(
+  "hbs",
+  handlebars.engine({
+    extname: "hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "./src/views");
+
+app.get("/", (req, res) => {
+  res.send("lll");
+});
+
+app.listen(5000, () => {
+  console.log("Server is listening on http://localhost:5000...");
+});
