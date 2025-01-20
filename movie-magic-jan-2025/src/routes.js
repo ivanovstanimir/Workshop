eb735107b3cbb605a79c123198b9a652dd9e5e38
IvanovStr <163607151+IvanovStr@users.noreply.github.com>
2025-01-20 13:31:56 +0200

    Add controller and page

--- movie-magic-jan-2025/src/routes.js
+++ movie-magic-jan-2025/src/routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+
 import homeController from "./contrllers/home-controller.js";
+import movieController from "./contrllers/movie-controller.js";
 
 const routes = Router();
 
 routes.use(homeController);
+routes.use(movieController);
 
 routes.get("*", (req, res) => {
   res.render("404");
