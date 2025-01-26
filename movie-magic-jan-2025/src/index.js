50989282ad21aa7f03b73786a3ad53d0b5b8455f
IvanovStr <163607151+IvanovStr@users.noreply.github.com>
2025-01-26 14:24:54 +0200

    Setup mongoose

--- movie-magic-jan-2025/src/index.js
+++ movie-magic-jan-2025/src/index.js
@@ -1,10 +1,21 @@
 import express from "express";
 import handlebars from "express-handlebars";
+import mongoose from "mongoose";
 
 import routes from "./routes.js";
 import showRatingHelper from "./helpers/rating-helper.js";
 
 const app = express();
+
+try {
+  const uri = "mongodb://localhost:27017/magic-movies-jan2025";
+  await mongoose.connect(uri);
+
+  console.log("DB Connected Successfuly");
+} catch (err) {
+  console.log("Cannot connest to DB");
+  console.error(err.message);
+}
 
 app.engine(
   "hbs",
