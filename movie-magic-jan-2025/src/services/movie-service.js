858aa8f91b6b3b32fba5f052f8a51f040c35057b
IvanovStr <163607151+IvanovStr@users.noreply.github.com>
2025-01-26 16:02:37 +0200

    Refactor home page to use db movies

--- movie-magic-jan-2025/src/services/movie-service.js
+++ movie-magic-jan-2025/src/services/movie-service.js
@@ -1,40 +1,43 @@
-import { v4 as uuid } from 'uuid';
-import movies from "../movies.js";
+import { v4 as uuid } from "uuid";
+// import movies from "../movies.js";
+import Movie from "../models/Movie.js";
 
 export default {
-    getAll(filter = {}) {
-        let result = movies;
+  getAll(filter = {}) {
+    let result = Movie.find({});
 
-        if (filter.search) {
-            result = result.filter(movie => movie.title.toLowerCase().includes(filter.search.toLowerCase()));
-        }
+    // if (filter.search) {
+    //   result = result.filter((movie) =>
+    //     movie.title.toLowerCase().includes(filter.search.toLowerCase())
+    //   );
+    // }
 
-        if (filter.genre) {
-            result = result.filter(movie => movie.genre.toLowerCase() === filter.genre);
-        }
+    // if (filter.genre) {
+    //   result = result.filter(
+    //     (movie) => movie.genre.toLowerCase() === filter.genre
+    //   );
+    // }
 
-        if (filter.year) {
-            result = result.filter(movie => movie.year === filter.year);
-        }
+    // if (filter.year) {
+    //   result = result.filter((movie) => movie.year === filter.year);
+    // }
 
-        return result;
-    },
-    findOne(movieId) {
-        // TODO: if movie is missing?
+    return result;
+  },
+  getOne(movieId) {
+    const result = Movie.findById(movieId);
 
-        const result = movies.find(movie => movie.id === movieId);
+    return result;
+  },
+  create(movieData) {
+    const newId = uuid();
 
-        return result;
-    },
-    create(movieData) {
-        const newId = uuid();
+    movies.push({
+      id: newId,
+      ...movieData,
+      rating: Number(movieData.rating),
+    });
 
-        movies.push({
-            id: newId,
-            ...movieData,
-            rating: Number(movieData.rating),
-        });
-
-        return newId;
-    }
-}
+    return newId;
+  },
+};
