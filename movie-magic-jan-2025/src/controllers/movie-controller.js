bb898f4a72306696328e6a8d89e75b30d4409d5d
IvanovStr <163607151+IvanovStr@users.noreply.github.com>
2025-01-26 16:03:21 +0200

    Fix movie details page

--- movie-magic-jan-2025/src/controllers/movie-controller.js
+++ movie-magic-jan-2025/src/controllers/movie-controller.js
@@ -3,30 +3,30 @@ import movieService from "../services/movie-service.js";
 
 const movieController = Router();
 
-movieController.get('/search', (req, res) => {
-    const filter = req.query;
-    const movies = movieService.getAll(filter);
+movieController.get("/search", (req, res) => {
+  const filter = req.query;
+  const movies = movieService.getAll(filter);
 
-    res.render('search', { movies, filter });
+  res.render("search", { movies, filter });
 });
 
-movieController.get('/create', (req, res) => {
-    res.render('create');
+movieController.get("/create", (req, res) => {
+  res.render("create");
 });
 
-movieController.post('/create', (req, res) => {
-    const newMovie = req.body;
+movieController.post("/create", (req, res) => {
+  const newMovie = req.body;
 
-    movieService.create(newMovie);
+  movieService.create(newMovie);
 
-    res.redirect('/');
+  res.redirect("/");
 });
 
-movieController.get('/:movieId/details', (req, res) => {
-    const movieId = req.params.movieId;
-    const movie = movieService.findOne(movieId);
+movieController.get("/:movieId/details", async (req, res) => {
+  const movieId = req.params.movieId;
+  const movie = await movieService.getOne(movieId);
 
-    res.render('details', { movie });
+  res.render("details", { movie });
 });
 
 export default movieController;
