053853df7cea2b0424eea89565d4136c2f722557
IvanovStr <163607151+IvanovStr@users.noreply.github.com>
2025-01-26 16:30:28 +0200

    refactor search page and functionality

--- movie-magic-jan-2025/src/services/movie-service.js
+++ movie-magic-jan-2025/src/services/movie-service.js
@@ -4,25 +4,20 @@ import Movie from "../models/Movie.js";
 
 export default {
   getAll(filter = {}) {
-    let result = Movie.find({});
+    let query = Movie.find({});
+    if (filter.search) {
+      query = query.where({ title: filter.search });
+    }
 
-    // if (filter.search) {
-    //   result = result.filter((movie) =>
-    //     movie.title.toLowerCase().includes(filter.search.toLowerCase())
-    //   );
-    // }
+    if (filter.genre) {
+      query = query.where({ genre: filter.genre });
+    }
 
-    // if (filter.genre) {
-    //   result = result.filter(
-    //     (movie) => movie.genre.toLowerCase() === filter.genre
-    //   );
-    // }
+    if (filter.year) {
+      query = query.where({ year: Number(filter.year) });
+    }
 
-    // if (filter.year) {
-    //   result = result.filter((movie) => movie.year === filter.year);
-    // }
-
-    return result;
+    return query;
   },
   getOne(movieId) {
     const result = Movie.findById(movieId);
